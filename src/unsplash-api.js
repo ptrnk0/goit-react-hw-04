b8a7c9bc1e294c0fc52449e100a7d9aec43f0f66
ptrnk0 <vlad.ptrnko@gmail.com>
2024-12-27 17:56:33 +0200

    refactor Unsplash API integration to set base URL and authorization headers

--- src/unsplash-api.js
+++ src/unsplash-api.js
@@ -3,18 +3,17 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_API_URL;
 
-axios.defaults.url = apiUrl;
+axios.defaults.baseURL = apiUrl;
+axios.defaults.headers.common["Authorization"] = `Client-ID ${apiKey}`;
+axios.defaults.headers.common["Accept-Version"] = "v1";
 
-const getImages = (userInput) => {
-	const response = axios.get(`/search/photos/`, {
-		headers: {
-			Authorization: `Client-ID ${apiKey}`,
-		},
-		param: {
+const getImages = async (userInput) => {
+	const response = await axios.get(`/search/photos`, {
+		params: {
 			query: userInput,
 		},
 	});
-	console.log(response);
+	console.log(response.data);
 
 	return response.data;
 };
