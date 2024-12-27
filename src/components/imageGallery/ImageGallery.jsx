4bd647abe535c236addde945f674a94c02fc327b
ptrnk0 <vlad.ptrnko@gmail.com>
2024-12-27 20:00:51 +0200

    refactor ImageGallery to render ImageCard components

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,7 +1,15 @@
-const ImageGallery = () => {
+import ImageCard from "../imageCard/ImageCard";
+
+const ImageGallery = ({ images }) => {
 	return (
 		<ul>
-			<li></li>
+			{images.map(({ id, urls, description }) => {
+				return (
+					<li key={id}>
+						<ImageCard urls={urls} description={description} />
+					</li>
+				);
+			})}
 		</ul>
 	);
 };
