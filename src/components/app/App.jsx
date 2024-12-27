b758de8823cfb873252bfa4cd208924592b5fc44
ptrnk0 <vlad.ptrnko@gmail.com>
2024-12-27 16:51:13 +0200

    add SearchBar component and integrate react-hot-toast for notifications

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import SearchBar from "../searchBar/SearchBar";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+	return (
+		<>
+			<Toaster />
+			<SearchBar />
+		</>
+	);
+};
+
+export default App;
---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import toast from "react-hot-toast";
+
+const errorNotify = () => toast.error("The field must not be empty");
+
+const SearchBar = ({ onSubmit }) => {
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		const userInput = evt.target.elements.search.value;
+
+		if (userInput.trim() === "") {
+			errorNotify();
+			return;
+		}
+
+		onSubmit(userInput);
+	};
+
+	return (
+		<header>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					autoComplete="off"
+					autoFocus
+					placeholder="Search images and photos"
+					name="search"
+				/>
+				<button type="submit">Search</button>
+			</form>
+		</header>
+	);
+};
+
+export default SearchBar;
