4453c5a3809ebe8a6ce6812d512d823fc46b8472
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 15:39:49 +0200

    add modal implementation to ImageGallery component

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,13 +1,24 @@
 import css from "./ImageGallery.module.css";
 import ImageCard from "../imageCard/ImageCard";
+import ImageModal from "../imageModal/ImageModal";
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images, onCloseModal, onOpenModal, value }) => {
 	return (
 		<ul className={css.galleryContainer}>
 			{images.map(({ id, urls, description }) => {
 				return (
 					<li key={id}>
-						<ImageCard urls={urls} description={description} />
+						<ImageCard
+							urls={urls}
+							description={description}
+							onOpenModal={onOpenModal}
+						/>
+						<ImageModal
+							urls={urls}
+							description={description}
+							onCloseModal={onCloseModal}
+							value={value}
+						/>
 					</li>
 				);
 			})}
