c8d485ef80a45646d7c72aa245ebad76243b2e3f
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 15:39:42 +0200

    refactor ImageCard component to use onOpenModal prop and remove modal implementation

--- src/components/imageCard/ImageCard.jsx
+++ src/components/imageCard/ImageCard.jsx
@@ -1,40 +1,8 @@
-import ImageModal from "../imageModal/ImageModal";
-import { useState } from "react";
-import css from "./ImageCard.module.css";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
-
-const ImageCard = ({ urls, description }) => {
-	const [modalIsOpen, setIsOpen] = useState(false);
-
-	const handleOpenModal = () => {
-		setIsOpen(true);
-	};
-
-	const handleCloseModal = () => {
-		setIsOpen(false);
-	};
-
+const ImageCard = ({ urls, description, onOpenModal }) => {
 	return (
-		<>
-			<div onClick={handleOpenModal}>
-				<img src={urls.small} alt={description} />
-			</div>
-			<Modal
-				isOpen={modalIsOpen}
-				onRequestClose={handleCloseModal}
-				contentLabel="Modal window for image"
-				className={css.modalContent}
-				overlayClassName={css.modalOverlay}
-			>
-				<ImageModal
-					urls={urls.regular}
-					description={description}
-					onClose={handleCloseModal}
-				></ImageModal>
-			</Modal>
-		</>
+		<div onClick={onOpenModal}>
+			<img src={urls.small} alt={description} />
+		</div>
 	);
 };
 
