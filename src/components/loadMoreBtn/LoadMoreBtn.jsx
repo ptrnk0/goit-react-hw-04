d0c4d80934059f700135993bd51c9ee1cd2686a5
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 18:24:48 +0200

    add LoadMoreBtn component with styles

--- src/components/loadMoreBtn/LoadMoreBtn.jsx
+++ src/components/loadMoreBtn/LoadMoreBtn.jsx
@@ -1,6 +1,8 @@
+import css from "./LoadMoreBtn.module.css";
+
 const LoadMoreBtn = ({ onClick }) => {
 	return (
-		<button type="button" onClick={onClick}>
+		<button type="button" onClick={onClick} className={css.loadMoreBtn}>
 			Load more
 		</button>
 	);
