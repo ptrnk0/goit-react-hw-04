410e1c97669f71a301540a55ecfc40c72d064fb3
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 12:49:07 +0200

    add styling to ImageGallery component

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,8 +1,9 @@
+import css from "./ImageGallery.module.css";
 import ImageCard from "../imageCard/ImageCard";
 
 const ImageGallery = ({ images }) => {
 	return (
-		<ul>
+		<ul className={css.galleryContainer}>
 			{images.map(({ id, urls, description }) => {
 				return (
 					<li key={id}>
