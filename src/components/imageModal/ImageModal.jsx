e73e6b50192917665fd8823c52e087856987cd96
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 15:39:58 +0200

    implement ImageModal component with styling and functionality

--- src/components/imageModal/ImageModal.jsx
+++ src/components/imageModal/ImageModal.jsx
@@ -1,10 +1,25 @@
 import css from "./ImageModal.module.css";
+import Modal from "react-modal";
 
-const ImageModal = ({ urls, description, onClose }) => {
+Modal.setAppElement("#root");
+
+const ImageModal = ({ urls, description, onCloseModal, value }) => {
 	return (
-		<div onClick={onClose} className={css.modalImgContainer}>
-			<img src={urls} alt={description} className={css.modalImg} />
-		</div>
+		<Modal
+			isOpen={value}
+			onRequestClose={onCloseModal}
+			contentLabel="Modal window for image"
+			className={css.modalContent}
+			overlayClassName={css.modalOverlay}
+		>
+			<div onClick={onCloseModal} className={css.modalImgContainer}>
+				<img
+					src={urls.regular}
+					alt={description}
+					className={css.modalImg}
+				/>
+			</div>
+		</Modal>
 	);
 };
 
