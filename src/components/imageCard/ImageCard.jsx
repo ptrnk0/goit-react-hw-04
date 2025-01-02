823f51846c2f7575e50ade6cbeeeef668b8bdf88
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 12:09:12 +0200

    add modal functionality to ImageCard component with styling

--- src/components/imageCard/ImageCard.jsx
+++ src/components/imageCard/ImageCard.jsx
@@ -1,8 +1,40 @@
+import ImageModal from "../imageModal/ImageModal";
+import { useState } from "react";
+import css from "./ImageCard.module.css";
+
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+
 const ImageCard = ({ urls, description }) => {
+	const [modalIsOpen, setIsOpen] = useState(false);
+
+	const handleOpenModal = () => {
+		setIsOpen(true);
+	};
+
+	const handleCloseModal = () => {
+		setIsOpen(false);
+	};
+
 	return (
-		<div>
-			<img src={urls.small} alt={description} />
-		</div>
+		<>
+			<div onClick={handleOpenModal}>
+				<img src={urls.small} alt={description} />
+			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={handleCloseModal}
+				contentLabel="Modal window for image"
+				className={css.modalContent}
+				overlayClassName={css.modalOverlay}
+			>
+				<ImageModal
+					urls={urls.regular}
+					description={description}
+					onClose={handleCloseModal}
+				></ImageModal>
+			</Modal>
+		</>
 	);
 };
 
