a79e8fb94f1ee7acdbcba7827ccdc9606e17a8fb
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-02 18:24:26 +0200

    refactor ImageGallery component and add styles for layout

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,24 +1,13 @@
 import css from "./ImageGallery.module.css";
 import ImageCard from "../imageCard/ImageCard";
-import ImageModal from "../imageModal/ImageModal";
 
-const ImageGallery = ({ images, onCloseModal, onOpenModal, value }) => {
+const ImageGallery = ({ images, onOpenModal }) => {
 	return (
 		<ul className={css.galleryContainer}>
-			{images.map(({ id, urls, description }) => {
+			{images.map((image) => {
 				return (
-					<li key={id}>
-						<ImageCard
-							urls={urls}
-							description={description}
-							onOpenModal={onOpenModal}
-						/>
-						<ImageModal
-							urls={urls}
-							description={description}
-							onCloseModal={onCloseModal}
-							value={value}
-						/>
+					<li key={image.id}>
+						<ImageCard image={image} onOpenModal={onOpenModal} />
 					</li>
 				);
 			})}
