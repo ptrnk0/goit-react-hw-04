4a15088a1a454eafcefa2102edded4daac8be680
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-03 16:57:29 +0200

    refactor ImageGallery to use forwardRef

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,18 +1,24 @@
 import css from "./ImageGallery.module.css";
 import ImageCard from "../imageCard/ImageCard";
+import { forwardRef } from "react";
 
-const ImageGallery = ({ images, onOpenModal }) => {
+const ImageGallery = forwardRef((props, ref) => {
 	return (
-		<ul className={css.galleryContainer}>
-			{images.map((image) => {
+		<ul className={css.galleryContainer} ref={ref}>
+			{props.images.map((image) => {
 				return (
 					<li key={image.id}>
-						<ImageCard image={image} onOpenModal={onOpenModal} />
+						<ImageCard
+							image={image}
+							onOpenModal={props.onOpenModal}
+						/>
 					</li>
 				);
 			})}
 		</ul>
 	);
-};
+});
+
+ImageGallery.displayName = "ImageGallery";
 
 export default ImageGallery;
