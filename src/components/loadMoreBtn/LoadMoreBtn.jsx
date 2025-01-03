dd423ef840519a783c36fe000f47ef744c1b7e2c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-03 16:56:54 +0200

    refactor LoadMoreBtn to use forwardRef

--- src/components/loadMoreBtn/LoadMoreBtn.jsx
+++ src/components/loadMoreBtn/LoadMoreBtn.jsx
@@ -1,11 +1,19 @@
+import { forwardRef } from "react";
 import css from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ onClick }) => {
+const LoadMoreBtn = forwardRef((props, ref) => {
 	return (
-		<button type="button" onClick={onClick} className={css.loadMoreBtn}>
+		<button
+			type="button"
+			onClick={props.onClick}
+			className={css.loadMoreBtn}
+			ref={ref}
+		>
 			Load more
 		</button>
 	);
-};
+});
+
+LoadMoreBtn.displayName = "LoadMoreBtn";
 
 export default LoadMoreBtn;
